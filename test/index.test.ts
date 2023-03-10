c48cbd5dd5143246241750fc8f340898ee41a9c8
abdelhameedalsayed <abdulhameed.azab@gmail.com>
2023-03-10 23:35:53 +0200

    adding testing function

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,24 +1,18 @@
-import { afterAll, beforeAll, describe, expect, test } from '@jest/globals';
-import fetch from 'node-fetch';
-import { createTestServer } from '../.wundergraph/generated/testing';
+import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import fetch from "node-fetch";
+import { createTestServer } from "../.wundergraph/generated/testing";
 
 const wg = createTestServer({ fetch: fetch as any });
 beforeAll(() => wg.start());
 afterAll(() => wg.stop());
 
-describe('test Countries API', () => {
-	test('country by code', async () => {
-		const result = await wg.client().query({
-			operationName: 'Country',
-		});
-		const country = result.data?.countries_country;
-		expect(country?.capital).toBe('Berlin');
-	});
-
-	test('continents', async () => {
-		const result = await wg.client().query({
-			operationName: 'Continents',
-		});
-		expect(result.data?.countries_continents.length).toBe(7);
-	});
+describe("test Countries API", () => {
+  test("geojson", async () => {
+    const result = await wg.client().query({
+      operationName: "Location",
+    });
+    const pointCoordArrLength =
+      result.data?.geojson?.features[0].geometry.coordinates[0][0][0].length;
+    expect(pointCoordArrLength).toBe(2);
+  });
 });
